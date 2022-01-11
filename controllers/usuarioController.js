cead273a0a12534c54e7baf2ed6908e6ea5f610a
aitorulz1 <quabbala@aitor.local>
2022-01-11 15:24:15 +0100

    Setting user login

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,40 @@
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
+exports.crearUsuario = async(req, res) => {
 
-exports.crearUsuario = (req, res) => {
-    // console.log(req.body)
+    // Revisar si hay errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()})
+    }
+    
+    // Extraer mail y password
+    const { email, password } = req.body;
+
+    try {
+        // Revisar usuario unico
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({msg:'El usuario ya existe'})
+        }
+
+        // crear el usuario
+        usuario = new Usuario(req.body);
+
+        // Hashear el password
+        const salt = await bcrypt.genSalt(10);
+        usuario.password = await bcrypt.hash(password, salt);
+
+        //guardar usuario
+        await usuario.save();
+
+        //Mensaje de confirmación
+        res.send('Usuario creado correctamente');
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error')
+    }
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const usuarioController = require('../controllers/usuarioController')
+const { check } = require('express-validator')
 
 // Crear usuario
-router.post('/', usuarioController.crearUsuario);
+router.post('/', 
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El mail debe de ser válido').isEmail(),
+        check('password', 'El nombre es obligatorio').isLength({min: 3}),
+    ] ,usuarioController.crearUsuario);
 
 module.exports = router;
