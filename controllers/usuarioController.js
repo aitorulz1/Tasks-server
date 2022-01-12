47fd6a43f80456c5aa69b69b347c1d31ff2ce8bf
aitorulz1 <quabbala@aitor.local>
2022-01-12 12:03:53 +0100

    JWT

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,40 +1,58 @@
 const Usuario = require('../models/Usuario');
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
-exports.crearUsuario = async(req, res) => {
+exports.crearUsuario =  async (req, res) => {
 
     // Revisar si hay errores
-    const errores = validationResult(req);
+    const errores = validationResult(req); // req -> request para retornar si hay algún error y lo genera como un array
     if(!errores.isEmpty()){
         return res.status(400).json({errores: errores.array()})
     }
-    
-    // Extraer mail y password
-    const { email, password } = req.body;
 
-    try {
-        // Revisar usuario unico
-        let usuario = await Usuario.findOne({ email });
+    // Extraer email y password
+    const { email, password } = req.body; 
+
+    try{
+        // Revisar q el usuario registrado sea único 
+        let usuario = await Usuario.findOne({email});
 
         if(usuario) {
-            return res.status(400).json({msg:'El usuario ya existe'})
+           return res.status(400).json({msg: 'El usuario ya existe'})
         }
 
-        // crear el usuario
-        usuario = new Usuario(req.body);
+        // crea el usuario
+        usuario = new Usuario(req.body) ;
 
         // Hashear el password
-        const salt = await bcrypt.genSalt(10);
-        usuario.password = await bcrypt.hash(password, salt);
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
 
-        //guardar usuario
+        // Guardar Usuario
         await usuario.save();
 
-        //Mensaje de confirmación
-        res.send('Usuario creado correctamente');
-    } catch (error) {
+        // Crea y firmar el Json Web Token
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // Firmar el jwt
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn:3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+             // Mensaje de confirmación
+             res.json({ token });
+        });
+
+      
+    }catch(error){
         console.log(error);
-        res.status(400).send('Hubo un error')
+        res.status(400).send('Hubo un error');
+
     }
-}
+} 
